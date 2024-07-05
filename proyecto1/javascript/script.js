faa2896ccb38bd4f2bb2a86cf11750e46b982485
dayanrb <yeneve04.abc@gmail.com>
2024-07-05 14:40:14 -0400

    Add files via upload
    principiante

---
+++ proyecto1/javascript/script.js
@@ -0,0 +1,8 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar');
+    if (sidebar.style.left === '0px') {
+        sidebar.style.left = '-250px';
+    } else {
+        sidebar.style.left = '0px';
+    }
+});
