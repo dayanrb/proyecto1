848e7e8970d503d5ca716843463ef15ae5b75aa7
dayanrb <yeneve04.abc@gmail.com>
2024-07-07 22:28:17 -0400

    hi

--- proyecto1/javascript/script.js
+++ proyecto1/javascript/script.js
@@ -1,8 +1,10 @@
+// script.js
 document.getElementById('menu-icon').addEventListener('click', function() {
     var sidebar = document.getElementById('sidebar');
-    if (sidebar.style.left === '0px') {
-        sidebar.style.left = '-250px';
-    } else {
-        sidebar.style.left = '0px';
-    }
+    sidebar.style.left = '0px';
+});
+
+document.getElementById('close-btn').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar');
+    sidebar.style.left = '-250px';
 });
